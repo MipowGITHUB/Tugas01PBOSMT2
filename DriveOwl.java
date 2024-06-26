9c68fa0c54568f9498bf913afaa5e08e954bc786
Ahmad Nur Mu'minin <145762986+MipowGITHUB@users.noreply.github.com>
2024-06-26 22:20:04 +0700

    Upload Tugas 01 PBO - Kelas Hewan dan behavior

---
+++ DriveOwl.java
@@ -0,0 +1,10 @@
+public class DriveOwl {
+    public static void main(String[] args) {
+        Owl owL = new Owl();
+        owL.TampilkanOwl();
+        owL.makananOwl();
+        owL.UmurOwl();
+        owL.JenisOwl();
+    }
+}
+
